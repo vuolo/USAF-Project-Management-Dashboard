705a5aa188118e5c934d758bd05fc899544b8064
michaelvuolo <michaelvuolo1@gmail.com>
2023-02-26 15:32:40 -0500

    minor spacing adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,27 +17,27 @@ const Home: NextPage = () => {
 
             <div className="flex items-center justify-between gap-6 px-8 pt-4 pb-6">
               {/* Green */}
-              <div className="h-28 w-40 rounded-sm border border-black bg-[green] px-4 py-2 text-center text-white">
+              <div className="h-[7.5rem] w-40 rounded-sm border border-black bg-[green] px-4 py-2 text-center text-white">
                 <h3 className="h-12 text-xs">
                   Tracked Dependencies With &gt;5 Days Schedule Margin
                 </h3>
-                <h1 className="pt-4 text-2xl">2</h1>
+                <h1 className="pt-4 text-2xl font-medium">2</h1>
               </div>
 
               {/* Yellow */}
-              <div className="h-28 w-40 rounded-sm border border-black bg-[yellow] px-4 py-2 text-center text-black">
+              <div className="h-[7.5rem] w-40 rounded-sm border border-black bg-[yellow] px-4 py-2 text-center text-black">
                 <h3 className="h-12 text-xs">
                   Tracked Dependencies With &lt;5 Days Schedule Margin
                 </h3>
-                <h1 className="pt-4 text-2xl">1</h1>
+                <h1 className="pt-4 text-2xl font-medium">1</h1>
               </div>
 
               {/* Red */}
-              <div className="h-28 w-40 rounded-sm border border-black bg-[red] px-4 py-2 text-center text-white">
+              <div className="h-[7.5rem] w-40 rounded-sm border border-black bg-[red] px-4 py-2 text-center text-white">
                 <h3 className="h-12 text-xs">
                   Tracked Dependencies With Tracked Impacts
                 </h3>
-                <h1 className="pt-4 text-2xl">1</h1>
+                <h1 className="pt-4 text-2xl font-medium">1</h1>
               </div>
             </div>
 
@@ -59,7 +59,7 @@ const Home: NextPage = () => {
               {/* Green */}
               <div className="h-28 w-40 rounded-sm border border-black bg-[green] px-4 py-2 text-center text-white">
                 <h3 className="h-12 text-xs">Tracked Milestones on Schedule</h3>
-                <h1 className="pt-4 text-2xl">2</h1>
+                <h1 className="pt-2 text-2xl font-medium">2</h1>
               </div>
 
               {/* Yellow */}
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
                 <h3 className="h-12 text-xs">
                   Tracked Milestones Within 5 Days
                 </h3>
-                <h1 className="pt-4 text-2xl">0</h1>
+                <h1 className="pt-2 text-2xl font-medium">0</h1>
               </div>
 
               {/* Red */}
@@ -75,7 +75,7 @@ const Home: NextPage = () => {
                 <h3 className="h-12 text-xs">
                   Tracked Milestones Behind Schedule
                 </h3>
-                <h1 className="pt-4 text-2xl">2</h1>
+                <h1 className="pt-2 text-2xl font-medium">2</h1>
               </div>
             </div>
           </div>
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
               <h1>Financial Summary</h1>
             </div>
 
-            <div className="flex h-[26rem] items-center justify-between gap-6 px-8 pt-4 pb-6"></div>
+            <div className="flex h-[26.5rem] items-center justify-between gap-6 px-8 pt-4 pb-6"></div>
           </div>
         </div>
       </main>
