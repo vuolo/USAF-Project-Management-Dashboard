aaaccc468fb8340f983af79a307bba9aca423f87
michaelvuolo <michaelvuolo1@gmail.com>
2023-03-03 15:56:35 -0500

    updated url (/dependency -> /dependency-summary)

--- src/components/summaries/dependency-summary.tsx
+++ src/components/summaries/dependency-summary.tsx
@@ -45,7 +45,7 @@ function DependencySummary() {
       </div>
 
       <Link
-        href="/dependency"
+        href="/dependency-summary"
         className="mt-2 inline-flex items-center justify-center rounded-md border border-brand-dark bg-white px-4 py-2 text-sm font-medium text-brand-dark shadow-sm hover:bg-brand-dark hover:text-white focus:outline-none focus:ring-2 focus:ring-brand-dark focus:ring-offset-2 sm:w-auto"
       >
         See Dependencies
