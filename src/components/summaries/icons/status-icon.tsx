d73a3876f145be44d849342134584ee91375bafb
maxwellgraeser <mgraeser22701@gmail.com>
2023-03-10 17:50:30 -0500

    Tooltips for status icons

--- src/components/summaries/icons/status-icon.tsx
+++ src/components/summaries/icons/status-icon.tsx
@@ -1,19 +1,29 @@
 import { AlertTriangle, Circle, Square } from "lucide-react";
+import { Tooltip } from "react-tooltip";
 
 function StatusIcon({ status }: { status: string }) {
   switch (status) {
     case "ONTRACK":
     case "ON-BUDGET":
     case "green":
-      return <Circle style={{ fill: "green", color: "green" }} />;
+      return (<div><Circle style={{ fill: "green", color: "green" }}
+        data-tooltip-id="icon-tooltip"
+        data-tooltip-content={status} />
+        <Tooltip id="icon-tooltip" style={{ opacity: 80 }} /></div>);
     case "BEHIND":
     case "UNDER":
     case "yellow":
-      return <Square style={{ fill: "orange", color: "orange" }} />;
+      return (<div><Square style={{ fill: "orange", color: "orange" }}
+        data-tooltip-id="icon-tooltip"
+        data-tooltip-content={status} />
+        <Tooltip id="icon-tooltip" style={{ opacity: 80 }} /></div>);
     case "REALLY-BEHIND":
     case "OVER":
     case "red":
-      return <AlertTriangle style={{ fill: "red", color: "red" }} />;
+      return (<div><AlertTriangle style={{ fill: "red", color: "red" }}
+        data-tooltip-id="icon-tooltip"
+        data-tooltip-content={status} />
+        <Tooltip id="icon-tooltip" style={{ opacity: 80 }} /></div>);
     default:
       return <h1>...</h1>; //<></>; //<h1>N/A</h1>;
   }
