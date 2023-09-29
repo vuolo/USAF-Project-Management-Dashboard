2721a52ad6a01be6438ba4c02f2ebebc49c12424
michaelvuolo <michaelvuolo1@gmail.com>
2023-09-28 22:10:47 -0400

    some changes

--- src/utils/format_dependencies.tsx
+++ src/utils/format_dependencies.tsx
@@ -1,17 +1,47 @@
-import Link from "next/link"
-import { CountedDependency } from "~/types/milestone"
-import { classNames } from "./misc"
+import Link from "next/link";
+import { CountedDependency } from "~/types/milestone";
+import { classNames } from "./misc";
 
-export const getDateDifferenceComponent = (date_difference: number | bigint) => 
-<span><span className="font-bold">{Math.abs(Number(date_difference))} Days</span> {date_difference > 0 ? "left" : "over"}</span>
+export const getDateDifferenceComponent = (
+  date_difference: number | bigint
+) => (
+  <span>
+    <span className="font-bold">
+      {Math.abs(Number(date_difference))} Day
+      {Math.abs(Number(date_difference)) !== 1 ? "s" : ""}
+    </span>{" "}
+    {date_difference > 0 ? "left" : "over"}
+  </span>
+);
 
-export const formatDependencies = (dependencies?: CountedDependency[] | null | undefined) => dependencies && dependencies?.length > 0 ? dependencies?.map((dependency, index) => (
-    <div className={classNames("flex justify-between text-sm py-3 items-start px-4", index % 2 === 1 ? "bg-[#F7F7F7]" : "bg-white")} key={index}>
-      <div className="flex flex-col">
-        <Link href={`/dependencies?highlightDependencies=${dependency.pred_milestone_id}_to_${dependency.succ_milestone_id}`} className="text-[#2767C8] underline w-fit">{dependency.pred_milestone_name}{` → ${dependency.succ_milestone_name}`}</Link>
-        {/* TODO: Make BOTH project names clickable to go to the respective project page */}
-        <span className="text-left text-[#6A6A6A] text-xs">{`${dependency.pred_project_name} → ${dependency.succ_project_name}`}</span>
+export const formatDependencies = (
+  dependencies?: CountedDependency[] | null | undefined
+) =>
+  dependencies && dependencies?.length > 0 ? (
+    dependencies?.map((dependency, index) => (
+      <div
+        className={classNames(
+          "flex items-start justify-between py-3 px-4 text-sm",
+          index % 2 === 1 ? "bg-[#F7F7F7]" : "bg-white"
+        )}
+        key={index}
+      >
+        <div className="flex flex-col">
+          <Link
+            href={`/dependencies?highlightDependencies=${dependency.pred_milestone_id}_to_${dependency.succ_milestone_id}`}
+            className="w-fit text-[#2767C8] underline"
+          >
+            {dependency.pred_milestone_name}
+            {` → ${dependency.succ_milestone_name}`}
+          </Link>
+          {/* TODO: Make BOTH project names clickable to go to the respective project page */}
+          <span className="text-left text-xs text-[#6A6A6A]">{`${dependency.pred_project_name} → ${dependency.succ_project_name}`}</span>
+        </div>
+        {getDateDifferenceComponent(dependency.date_difference)}
       </div>
-      {getDateDifferenceComponent(dependency.date_difference)}
+    ))
+  ) : (
+    <div className="flex items-center justify-center bg-white py-3 px-4 text-sm text-[#6A6A6A]">
+      NO DEPENDENCIES TO DISPLAY...
     </div>
-  )) : <div className="flex justify-center text-sm py-3 items-center px-4 bg-white text-[#6A6A6A]">NO DEPENDENCIES TO DISPLAY...</div>
+  );
--- src/utils/format_project_schedule_summary.tsx
+++ src/utils/format_project_schedule_summary.tsx
@@ -1,18 +1,47 @@
-import Link from "next/link"
+import Link from "next/link";
 // import { CountedMilestone } from "~/types/milestone"
-import { classNames } from "./misc"
-import { ProjectMilestoneSummary } from "~/types/milestone"
+import { classNames } from "./misc";
+import { ProjectMilestoneSummary } from "~/types/milestone";
 
-export const getDateDifferenceComponent = (date_difference?: number | bigint) => date_difference !== undefined && date_difference !== null ?
-<span><span className="font-bold">{Math.abs(Number(date_difference))} Days</span> {date_difference > 0 ? "ahead" : "behind"}</span> : <span className="font-bold">N/A</span>
+export const getDateDifferenceComponent = (date_difference?: number | bigint) =>
+  date_difference !== undefined && date_difference !== null ? (
+    <span>
+      <span className="font-bold">
+        {Math.abs(Number(date_difference))} Day
+        {Math.abs(Number(date_difference)) !== 1 ? "s" : ""}
+      </span>{" "}
+      {date_difference < 0 ? "ahead" : "behind"}
+    </span>
+  ) : (
+    <span className="font-bold">N/A</span>
+  );
 
-export const formatProjectScheduleSummary = (projectMilestoneSummaries?: ProjectMilestoneSummary[] | null | undefined) => projectMilestoneSummaries && projectMilestoneSummaries?.length > 0 ? projectMilestoneSummaries?.map((projectMilestoneSummary, index) => (
-    <div className={classNames("flex justify-between text-sm py-3 items-start px-4", index % 2 === 1 ? "bg-[#F7F7F7]" : "bg-white")} key={index}>
-      <div className="flex flex-col">
-      <Link href={`?hightlightProjects=${projectMilestoneSummary.id}`} className="text-[#2767C8] underline w-fit">{projectMilestoneSummary.project_name}</Link>
-        <span className="text-left text-[#6A6A6A] text-xs">{`${projectMilestoneSummary.branch}`}</span>
+export const formatProjectScheduleSummary = (
+  projectMilestoneSummaries?: ProjectMilestoneSummary[] | null | undefined
+) =>
+  projectMilestoneSummaries && projectMilestoneSummaries?.length > 0 ? (
+    projectMilestoneSummaries?.map((projectMilestoneSummary, index) => (
+      <div
+        className={classNames(
+          "flex items-start justify-between py-3 px-4 text-sm",
+          index % 2 === 1 ? "bg-[#F7F7F7]" : "bg-white"
+        )}
+        key={index}
+      >
+        <div className="flex flex-col">
+          <Link
+            href={`?hightlightProjects=${projectMilestoneSummary.id}`}
+            className="w-fit text-[#2767C8] underline"
+          >
+            {projectMilestoneSummary.project_name}
+          </Link>
+          <span className="text-left text-xs text-[#6A6A6A]">{`${projectMilestoneSummary.branch}`}</span>
+        </div>
+        {getDateDifferenceComponent(projectMilestoneSummary.date_difference)}
       </div>
-      {getDateDifferenceComponent(projectMilestoneSummary.date_difference)}
+    ))
+  ) : (
+    <div className="flex items-center justify-center bg-white py-3 px-4 text-sm text-[#6A6A6A]">
+      NO PROJECTS TO DISPLAY...
     </div>
-  )) : <div className="flex justify-center text-sm py-3 items-center px-4 bg-white text-[#6A6A6A]">NO PROJECTS TO DISPLAY...</div>
-  
+  );
