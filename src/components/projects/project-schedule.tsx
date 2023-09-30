bfd705f34b5c0224827610eede5b355523e42863
michaelvuolo <michaelvuolo1@gmail.com>
2023-09-30 15:15:42 -0400

    today's date line now shows on all gantt charts.

--- src/components/projects/project-schedule.tsx
+++ src/components/projects/project-schedule.tsx
@@ -8,6 +8,8 @@ import TableProjectSchedule from "./tables/table-project-schedule";
 import type { view_project } from "~/types/view_project";
 import type { milestone } from "~/types/milestone";
 import ModalEditProjectSchedule from "./modals/modal-edit-project-schedule";
+import { sleep } from "~/utils/misc";
+import { displayTodayLine } from "~/utils/gantt";
 
 const columns = [
   { type: "string", label: "Task ID" },
@@ -16,7 +18,7 @@ const columns = [
   { type: "date", label: "End" },
   { type: "number", label: "Duration" },
   { type: "number", label: "Percent Complete" },
-  { type: "string", label: "Dependencies" }
+  { type: "string", label: "Dependencies" },
 ];
 
 function ProjectSchedule({ project }: { project: view_project }) {
@@ -24,34 +26,11 @@ function ProjectSchedule({ project }: { project: view_project }) {
   const { data: milestoneSchedules } = api.milestone.getSchedules.useQuery({
     project_id: project.id,
   });
-
   const [editScheduleModalOpen, setEditScheduleModalOpen] = useState(false);
-
-  // Display "Today" on the Gantt chark (hacky way to do it)
-  const chartRef = useRef(null);
-  useEffect(() => {
-    if (chartRef.current) {
-      let curMonthTextElement;
-      // while (!curMonthTextElement) {
-      //   // TODO: Find the first <text> element with a month name (Sep, Dec, Oct, etc.) in it that matches today's month name.
-      //   // curMonthTextElement = (chartRef.current as HTMLElement).querySelector("text")!;
-      // }
-
-      // // The chart's SVG element can be accessed using `chartRef.current.chart.container`.
-      // const svg = (chartRef.current as HTMLElement).querySelector("svg")!
-      
-      // // You'd then use Google Chart's methods or some calculations to determine 
-      // // where today's date would be on the chart. This part can be tricky and 
-      // // might require some trial and error, as well as knowledge of the Gantt 
-      // // chart's internals.
-  
-      // const xPositionForToday = calculateXPositionForToday(svg);
-  
-      // // Draw a vertical line at xPositionForToday using either pure JS or a library.
-      // drawVerticalLine(svg, xPositionForToday);
-    }
-  }, [milestoneSchedules]); // Redraw line whenever data changes
-  
+  useEffect(
+    () => displayTodayLine({ milestoneSchedules }),
+    [milestoneSchedules]
+  );
 
   return (
     <div className="rounded-md bg-white pb-6 text-center shadow-md">
@@ -68,7 +47,7 @@ function ProjectSchedule({ project }: { project: view_project }) {
           )}
       </div>
 
-      <div className="flex flex-col justify-around gap-2 px-4 pt-4 pb-2 text-left sm:px-6 sm:pt-6 md:flex-row">
+      <div className="flex flex-col justify-around gap-2 px-4 pb-2 pt-4 text-left sm:px-6 sm:pt-6 md:flex-row">
         {false ? (
           <p className="text-center italic">
             There is no milestone schedule data available for this project.
@@ -79,70 +58,103 @@ function ProjectSchedule({ project }: { project: view_project }) {
 
             {/* Display Errors */}
             <div className="w-full text-center">
-              {milestoneSchedules && milestoneSchedules.map((milestone, idx) => {
-                const milestoneStartDate = (milestone.ActualStart && new Date(milestone.ActualStart).getFullYear() !== 1969) 
-                  ? new Date(milestone.ActualStart)
-                  : (milestone.ProjectedStart && new Date(milestone.ProjectedStart).getFullYear() !== 1969 
-                    ? new Date(milestone.ProjectedStart)
-                    : null);
+              {milestoneSchedules &&
+                milestoneSchedules.map((milestone, idx) => {
+                  const milestoneStartDate =
+                    milestone.ActualStart &&
+                    new Date(milestone.ActualStart).getFullYear() !== 1969
+                      ? new Date(milestone.ActualStart)
+                      : milestone.ProjectedStart &&
+                        new Date(milestone.ProjectedStart).getFullYear() !==
+                          1969
+                      ? new Date(milestone.ProjectedStart)
+                      : null;
 
-                const milestoneProjectedStartDate = (milestone.ProjectedStart && new Date(milestone.ProjectedStart).getFullYear() !== 1969)
-                  ? new Date(milestone.ProjectedStart)
-                  : null;
-                
-                const milestoneProjectedEndDate = (milestone.ProjectedEnd && new Date(milestone.ProjectedEnd).getFullYear() !== 1969)
-                  ? new Date(milestone.ProjectedEnd)
-                  : null;
+                  const milestoneProjectedStartDate =
+                    milestone.ProjectedStart &&
+                    new Date(milestone.ProjectedStart).getFullYear() !== 1969
+                      ? new Date(milestone.ProjectedStart)
+                      : null;
 
-                const milestoneActualStartDate = (milestone.ActualStart && new Date(milestone.ActualStart).getFullYear() !== 1969)
-                  ? new Date(milestone.ActualStart)
-                  : null; 
-                
-                const milestoneActualEndDate = (milestone.ActualEnd && new Date(milestone.ActualEnd).getFullYear() !== 1969)
-                  ? new Date(milestone.ActualEnd)
-                  : null;
-                
-                const predecessorEndDate = milestone.Predecessors 
-                  ? getPredecessorEndDate(milestone.Predecessors, milestoneSchedules) 
-                  : null;
+                  const milestoneProjectedEndDate =
+                    milestone.ProjectedEnd &&
+                    new Date(milestone.ProjectedEnd).getFullYear() !== 1969
+                      ? new Date(milestone.ProjectedEnd)
+                      : null;
 
-                // Check if the projected start date is after the projected end date:
-                if (milestoneProjectedStartDate && milestoneProjectedEndDate && milestoneProjectedStartDate > milestoneProjectedEndDate) {
-                  return (
-                    <div key={idx} className="text-red-600">
-                      <span className="font-bold">Error:</span> Milestone "{milestone.Name}" 
-                      has a projected start date that is after its projected end date.
-                    </div>
-                  );
-                }
-                
-                // Check if the actual start date is after the actual end date:
-                if (milestoneActualStartDate && milestoneActualEndDate && milestoneActualStartDate > milestoneActualEndDate) {
-                  return (
-                    <div key={idx} className="text-red-600">
-                      <span className="font-bold">Error:</span> Milestone "{milestone.Name}" 
-                      has an actual start date that is after its actual end date.
-                    </div>
-                  );
-                }
+                  const milestoneActualStartDate =
+                    milestone.ActualStart &&
+                    new Date(milestone.ActualStart).getFullYear() !== 1969
+                      ? new Date(milestone.ActualStart)
+                      : null;
 
-                // for milestones that have a start date before their predecessor's end date:
-                if (milestoneStartDate && predecessorEndDate && milestoneStartDate < predecessorEndDate) {
-                  return (
-                    <div key={idx} className="text-orange-600">
-                      <span className="font-bold">Warning:</span> Milestone "{milestone.Name}" 
-                      starts before its predecessor milestone "{milestone.Predecessors_Name}" is complete.
-                    </div>
-                  );
-                }
-                return null;
-              })}
+                  const milestoneActualEndDate =
+                    milestone.ActualEnd &&
+                    new Date(milestone.ActualEnd).getFullYear() !== 1969
+                      ? new Date(milestone.ActualEnd)
+                      : null;
+
+                  const predecessorEndDate = milestone.Predecessors
+                    ? getPredecessorEndDate(
+                        milestone.Predecessors,
+                        milestoneSchedules
+                      )
+                    : null;
+
+                  // Check if the projected start date is after the projected end date:
+                  if (
+                    milestoneProjectedStartDate &&
+                    milestoneProjectedEndDate &&
+                    milestoneProjectedStartDate > milestoneProjectedEndDate
+                  ) {
+                    return (
+                      <div key={idx} className="text-red-600">
+                        <span className="font-bold">Error:</span> Milestone
+                        &quot;
+                        {milestone.Name}&quot; has a projected start date that
+                        is after its projected end date.
+                      </div>
+                    );
+                  }
+
+                  // Check if the actual start date is after the actual end date:
+                  if (
+                    milestoneActualStartDate &&
+                    milestoneActualEndDate &&
+                    milestoneActualStartDate > milestoneActualEndDate
+                  ) {
+                    return (
+                      <div key={idx} className="text-red-600">
+                        <span className="font-bold">Error:</span> Milestone
+                        &quot;
+                        {milestone.Name}&quot; has an actual start date that is
+                        after its actual end date.
+                      </div>
+                    );
+                  }
+
+                  // for milestones that have a start date before their predecessor's end date:
+                  if (
+                    milestoneStartDate &&
+                    predecessorEndDate &&
+                    milestoneStartDate < predecessorEndDate
+                  ) {
+                    return (
+                      <div key={idx} className="text-orange-600">
+                        <span className="font-bold">Warning:</span> Milestone
+                        &quot;
+                        {milestone.Name}&quot; starts before its predecessor
+                        milestone &quot;{milestone.Predecessors_Name}&quot; is
+                        complete.
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
             </div>
-
 
             {milestoneSchedules && milestoneSchedules.length !== 0 && (
               <Chart
-                ref={chartRef}
                 chartType="Gantt"
                 width="100%"
                 height="100%"
@@ -168,34 +180,43 @@ function ProjectSchedule({ project }: { project: view_project }) {
 export default ProjectSchedule;
 
 // Utility function to get the end date of the predecessor milestone
-const getPredecessorEndDate = (predecessorId: string, milestones: milestone[]): Date | null => {
-  const predecessor = milestones.find(m => m.ID.toString() === predecessorId);
-  return predecessor 
-    ? (predecessor.ActualEnd && new Date(predecessor.ActualEnd).getFullYear() !== 1969 
-        ? new Date(predecessor.ActualEnd)
-        : (predecessor.ProjectedEnd && new Date(predecessor.ProjectedEnd).getFullYear() !== 1969 
-            ? new Date(predecessor.ProjectedEnd)
-            : null))
+const getPredecessorEndDate = (
+  predecessorId: string,
+  milestones: milestone[]
+): Date | null => {
+  const predecessor = milestones.find((m) => m.ID.toString() === predecessorId);
+  return predecessor
+    ? predecessor.ActualEnd &&
+      new Date(predecessor.ActualEnd).getFullYear() !== 1969
+      ? new Date(predecessor.ActualEnd)
+      : predecessor.ProjectedEnd &&
+        new Date(predecessor.ProjectedEnd).getFullYear() !== 1969
+      ? new Date(predecessor.ProjectedEnd)
+      : null
     : null;
-
 };
 
 function GanttChartDataFormat(milestoneSchedules: milestone[]) {
   const rows: any[] = [];
 
   milestoneSchedules.map((milestoneSchedule) => {
-    let startDate = milestoneSchedule.ActualStart && new Date(milestoneSchedule.ActualStart).getFullYear() !== 1969 
-      ? new Date(milestoneSchedule.ActualStart)
-      : (milestoneSchedule.ProjectedStart && new Date(milestoneSchedule.ProjectedStart).getFullYear() !== 1969 
+    const startDate =
+      milestoneSchedule.ActualStart &&
+      new Date(milestoneSchedule.ActualStart).getFullYear() !== 1969
+        ? new Date(milestoneSchedule.ActualStart)
+        : milestoneSchedule.ProjectedStart &&
+          new Date(milestoneSchedule.ProjectedStart).getFullYear() !== 1969
         ? new Date(milestoneSchedule.ProjectedStart)
-        : null);
+        : null;
 
-    let endDate = milestoneSchedule.ActualEnd && new Date(milestoneSchedule.ActualEnd).getFullYear() !== 1969 
+    let endDate =
+      milestoneSchedule.ActualEnd &&
+      new Date(milestoneSchedule.ActualEnd).getFullYear() !== 1969
         ? new Date(milestoneSchedule.ActualEnd)
-        : (milestoneSchedule.ProjectedEnd && new Date(milestoneSchedule.ProjectedEnd).getFullYear() !== 1969 
-          ? new Date(milestoneSchedule.ProjectedEnd)
-          : null);
-
+        : milestoneSchedule.ProjectedEnd &&
+          new Date(milestoneSchedule.ProjectedEnd).getFullYear() !== 1969
+        ? new Date(milestoneSchedule.ProjectedEnd)
+        : null;
 
     // Check if start and end dates are the same
     if (startDate && endDate && startDate.getTime() === endDate.getTime()) {
@@ -212,7 +233,7 @@ function GanttChartDataFormat(milestoneSchedules: milestone[]) {
       getPercentage(milestoneSchedule),
       milestoneSchedule.Predecessors === null
         ? null
-        : milestoneSchedule.Predecessors.toString()
+        : milestoneSchedule.Predecessors.toString(),
     ]);
   });
 
@@ -223,42 +244,6 @@ function GanttChartDataFormat(milestoneSchedules: milestone[]) {
   return data;
 }
 
-// function calculateXPositionForToday(svg: SVGElement) {
-//   // Here's the tricky part. Gantt's internal representation must be understood.
-//   // For simplicity, let's assume the Gantt starts at the earliest milestone and ends at the latest.
-//   const milestonesDates = milestoneSchedules.map(m => [new Date(m.ProjectedStart), new Date(m.ProjectedEnd)]);
-//   const minDate = Math.min(...milestonesDates.map(d => d[0].getTime()));
-//   const maxDate = Math.max(...milestonesDates.map(d => d[1].getTime()));
-  
-//   // Calculate the position of today as a percentage
-//   const today = new Date().getTime();
-//   const positionPercentage = (today - minDate) / (maxDate - minDate);
-  
-//   // Convert percentage to actual x-position in the SVG
-//   // This might require adjustments based on padding/margins in the SVG
-//   const svgWidth = svg.viewBox.baseVal.width;
-//   const xPosition = svgWidth * positionPercentage;
-  
-//   return xPosition;
-// }
-
-// function drawVerticalLine(svg, xPosition) {
-//   const height = svg.viewBox.baseVal.height;
-
-//   // Create a new line element
-//   const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-//   line.setAttribute('x1', xPosition.toString());
-//   line.setAttribute('y1', '0');
-//   line.setAttribute('x2', xPosition.toString());
-//   line.setAttribute('y2', height.toString());
-//   line.setAttribute('stroke', 'red');  // Color of the line
-//   line.setAttribute('stroke-width', '2');  // Width of the line
-
-//   // Append the line to the SVG
-//   svg.appendChild(line);
-// }
-
-
 const getOptions = (cHeight: number) => {
   const options = {
     height: cHeight,
@@ -267,14 +252,14 @@ const getOptions = (cHeight: number) => {
       arrow: {
         angle: 100,
         width: 1,
-        color: 'dodgerblue',
-        radius: 0
+        color: "dodgerblue",
+        radius: 0,
       },
       criticalPathStyle: {
         stroke: "#e64a19",
       },
-      innerGridTrack: {fill: '#F3F7F9'},
-    innerGridDarkTrack: {fill: '#DCE6EC'}
+      innerGridTrack: { fill: "#F3F7F9" },
+      innerGridDarkTrack: { fill: "#DCE6EC" },
     },
   };
 
@@ -284,31 +269,32 @@ const getOptions = (cHeight: number) => {
 const getPercentage = (milestone: milestone) => {
   const currDate = new Date();
 
-  const validActualStart = milestone.ActualStart && new Date(milestone.ActualStart).getFullYear() !== 1969;
-  const validActualEnd = milestone.ActualEnd && new Date(milestone.ActualEnd).getFullYear() !== 1969;
-  const validProjectedEnd = milestone.ProjectedEnd && new Date(milestone.ProjectedEnd).getFullYear() !== 1969;
+  const validActualStart =
+    milestone.ActualStart &&
+    new Date(milestone.ActualStart).getFullYear() !== 1969;
+  const validActualEnd =
+    milestone.ActualEnd && new Date(milestone.ActualEnd).getFullYear() !== 1969;
+  const validProjectedEnd =
+    milestone.ProjectedEnd &&
+    new Date(milestone.ProjectedEnd).getFullYear() !== 1969;
 
-  if (!validActualStart && !validProjectedEnd) {
-    return 0;
-  } 
+  if (!validActualStart && !validProjectedEnd) return 0;
   else if (validActualEnd) {
     if (currDate < new Date(milestone.ActualEnd)) {
       const start = new Date(milestone.ActualStart);
       const end = new Date(milestone.ActualEnd);
       const daysElapsed = (+currDate - +start) / (+end - +start);
       return Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100));
-    }
-    else return 100;
-  } 
-  else if (validProjectedEnd) {
+    } else return 100;
+  } else if (validProjectedEnd) {
     if (currDate < new Date(milestone.ProjectedEnd)) {
       const start = new Date(milestone.ActualStart);
       const end = new Date(milestone.ProjectedEnd);
       const daysElapsed = (+currDate - +start) / (+end - +start);
-      return "Projected " + Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100)).toString();
-    }
-    else return "Projected 100";
-  }
-
-  else return 0;
+      return (
+        "Projected " +
+        Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100)).toString()
+      );
+    } else return "Projected 100";
+  } else return 0;
 };
--- src/components/summaries/dependency-graph.tsx
+++ src/components/summaries/dependency-graph.tsx
@@ -1,6 +1,8 @@
 import { Chart } from "react-google-charts";
 import { api } from "~/utils/api";
 import type { all_successors } from "~/types/all_successors";
+import { displayTodayLine } from "~/utils/gantt";
+import { useEffect } from "react";
 
 const columns = [
   { type: "string", label: "Task ID" },
@@ -19,6 +21,7 @@ function DependencyGraph() {
     all_successors && all_successors.length > 0
       ? all_successors.length * 42 + 172
       : 0;
+  useEffect(() => displayTodayLine({ all_successors }), [all_successors]);
 
   return (
     <div className="rounded-md bg-white pb-6 text-center shadow-md">
@@ -26,33 +29,42 @@ function DependencyGraph() {
         <h1>Dependency Graph</h1>
       </div>
 
-      <div className="flex flex-col gap-4 px-4 pt-4 pb-2 text-left sm:px-6 sm:pt-6">
+      <div className="flex flex-col gap-4 px-4 pb-2 pt-4 text-left sm:px-6 sm:pt-6">
         {/* Display Errors for each successor that has an end date date before the predeccessor's end date. */}
         <div className="w-full text-center">
-          {all_successors && all_successors.map((successor, idx) => {
-            const predEndDate = (successor.pred_actual_end && new Date(successor.pred_actual_end).getFullYear() !== 1969) 
-              ? new Date(successor.pred_actual_end)
-              : (successor.pred_proj_end && new Date(successor.pred_proj_end).getFullYear() !== 1969 
-                ? new Date(successor.pred_proj_end)
-                : null);
-        
-            const succStartDate = (successor.succ_actual_start && new Date(successor.succ_actual_start).getFullYear() !== 1969) 
-                ? new Date(successor.succ_actual_start)
-                : (successor.succ_proj_start && new Date(successor.succ_proj_start).getFullYear() !== 1969 
-                  ? new Date(successor.succ_proj_start)
-                  : null);
-        
+          {all_successors &&
+            all_successors.map((successor, idx) => {
+              const predEndDate =
+                successor.pred_actual_end &&
+                new Date(successor.pred_actual_end).getFullYear() !== 1969
+                  ? new Date(successor.pred_actual_end)
+                  : successor.pred_proj_end &&
+                    new Date(successor.pred_proj_end).getFullYear() !== 1969
+                  ? new Date(successor.pred_proj_end)
+                  : null;
 
-            if (succStartDate && predEndDate && succStartDate < predEndDate) {
-              return (
-                <div key={idx} className="text-orange-600">
-                  <span className="font-bold">Warning:</span> Successor task "{successor.succ_name}" in project "{successor.succ_proj_name}" 
-                  starts before its predecessor task "{successor.pred_name}" in project "{successor.pred_proj_name}" is complete.
-                </div>
-              );
-            }
-            return null;
-          })}
+              const succStartDate =
+                successor.succ_actual_start &&
+                new Date(successor.succ_actual_start).getFullYear() !== 1969
+                  ? new Date(successor.succ_actual_start)
+                  : successor.succ_proj_start &&
+                    new Date(successor.succ_proj_start).getFullYear() !== 1969
+                  ? new Date(successor.succ_proj_start)
+                  : null;
+
+              if (succStartDate && predEndDate && succStartDate < predEndDate) {
+                return (
+                  <div key={idx} className="text-orange-600">
+                    <span className="font-bold">Warning:</span> Successor task
+                    &quot;{successor.succ_name}&quot; in project &quot;
+                    {successor.succ_proj_name}&quot; starts before its
+                    predecessor task &quot;{successor.pred_name}&quot; in
+                    project &quot;{successor.pred_proj_name}&quot; is complete.
+                  </div>
+                );
+              }
+              return null;
+            })}
         </div>
 
         {!all_successors ? (
@@ -105,16 +117,19 @@ function GanttChartDataFormat(successors: all_successors[]) {
         successor.pred_name,
       ]);
     } else {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (rows[index][6] === null) rows[index][6] = successor.pred_name;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-plus-operands
       else rows[index][6] = rows[index][6] + ", " + successor.pred_name;
     }
     return 0;
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const data = [columns, ...rows];
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return data;
 }
-
 
 const getOptions = (cHeight: number) => {
   const options = {
@@ -123,23 +138,25 @@ const getOptions = (cHeight: number) => {
       arrow: {
         angle: 100,
         width: 1,
-        color: 'dodgerblue',
-        radius: 0
+        color: "dodgerblue",
+        radius: 0,
       },
       criticalPathStyle: {
         stroke: "#e64a19",
       },
     },
     height: cHeight,
-    innerGridTrack: {fill: '#F3F7F9'},
-    innerGridDarkTrack: {fill: '#DCE6EC'},
+    innerGridTrack: { fill: "#F3F7F9" },
+    innerGridDarkTrack: { fill: "#DCE6EC" },
   };
 
   return options;
 };
 
-
-const getDate = (actualDate: string | null | Date , projectedDate: string | Date): Date | null => {
+const getDate = (
+  actualDate: string | null | Date,
+  projectedDate: string | Date
+): Date | null => {
   if (actualDate && new Date(actualDate).getFullYear() !== 1969) {
     return new Date(actualDate);
   } else if (projectedDate && new Date(projectedDate).getFullYear() !== 1969) {
@@ -151,36 +168,30 @@ const getDate = (actualDate: string | null | Date , projectedDate: string | Date
 
 const getPercentage = (successor: all_successors, isPredecessor: boolean) => {
   const currDate = new Date();
-  const actualStartKey = isPredecessor ? 'pred_actual_start' : 'succ_actual_start';
-  const actualEndKey = isPredecessor ? 'pred_actual_end' : 'succ_actual_end';
-  const projEndKey = isPredecessor ? 'pred_proj_end' : 'succ_proj_end';
+  const actualStartKey = isPredecessor
+    ? "pred_actual_start"
+    : "succ_actual_start";
+  const actualEndKey = isPredecessor ? "pred_actual_end" : "succ_actual_end";
+  const projEndKey = isPredecessor ? "pred_proj_end" : "succ_proj_end";
 
-  if (successor[actualStartKey] === null)
-  {
+  if (successor[actualStartKey] === null) {
     return 0;
-  } 
-  else if (successor[actualEndKey] !== null)
-  {
-    if (currDate < new Date(successor[actualEndKey]))
-    {
+  } else if (successor[actualEndKey] !== null) {
+    if (currDate < new Date(successor[actualEndKey])) {
       const start = new Date(successor[actualStartKey]);
       const end = new Date(successor[actualEndKey]);
       const daysElapsed = (+currDate - +start) / (+end - +start);
       return Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100));
-    }
-    else return 100;
-  } 
-  else if (successor[projEndKey] !== null)
-  {
-    if (currDate < new Date(successor[projEndKey]))
-    {
+    } else return 100;
+  } else if (successor[projEndKey] !== null) {
+    if (currDate < new Date(successor[projEndKey])) {
       const start = new Date(successor[actualStartKey]);
       const end = new Date(successor[projEndKey]);
       const daysElapsed = (+currDate - +start) / (+end - +start);
-      return "Projected " + Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100)).toString();
-    }
-    else return "Projected 100";
-  }
-
-  else return 0;
+      return (
+        "Projected " +
+        Math.round(Math.min(Math.max(daysElapsed * 100, 0), 100)).toString()
+      );
+    } else return "Projected 100";
+  } else return 0;
 };
